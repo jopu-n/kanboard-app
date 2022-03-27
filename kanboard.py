91c7716c65894708a16a69b164737a06c8e352f4
MrGunsoul <nico.kranni@hotmail.com>
2022-03-27 19:46:20 +0300

    Updated kanboard.py
    Changed variables to better one, following Python Style Guide.

--- kanboard.py
+++ kanboard.py
@@ -1,27 +1,27 @@
-import itertools
-import datetime
-
+import itertools,datetime
 
 
 class MainTask:
+
     newid = itertools.count()
-    def __init__(self, deadline, endDate, worker):
-        self.ticketID = next(MainTask.newid)     
-        self.deadline=deadline
-        self.startDate=datetime.datetime.now()
-        self.endDate=endDate
-        self.worker=worker
-        
+
+    def __init__(self, deadline, end_date, worker):
+        self.ticket_id = next(MainTask.newid)
+        self.deadline = deadline
+        self.start_date = datetime.datetime.now()
+        self.end_date = end_date
+        self.worker = worker
+
     def __str__(self):
-        return "\nTicket ID: {0}\nStart date is: {1}\nDeadline is: {2}\nEnd Date is: {3}\nWho is doing the ticket: {4}\n".format(self.ticketID, self.startDate.strftime("%x"), self.deadline, self.endDate, self.worker)
+        return "\nTicket ID: {0}\nStart date is: {1}\nDeadline is: {2}\nEnd Date is: {3}\nWho is doing the ticket: {4}\n".format(self.ticket_id, self.start_date.strftime("%x"), self.deadline, self.end_date, self.worker)
 
-    def set_deadline (self, date):        
-        self.deadline=date
-    
-    def set_ticketID(self, ID):
-        self.ticketID=ID
+    def set_deadline(self, date):
+        self.deadline = date
+
+    def set_ticketid(self, id):
+        self.ticket_id = id
 
 
-test=MainTask("Tomorrow","Yesterday", "nico")
+test = MainTask("Tomorrow", "Yesterday", "nico")
 
 print(test)
