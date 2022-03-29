deb85f69a3ea3cfac97b2f7172309417ec54ec44
Nico Kranni <nico.kranni@hotmail.com>
2022-03-29 12:34:29 +0300

    New File and Class
    Added save.py that handles the loading and saving of the classes. Also in save.py we create objects

--- kanboard.py
+++ kanboard.py
@@ -1,6 +1,7 @@
-import itertools,datetime
+import itertools, datetime, json, pickle
 
-
+# Empty list for classes
+data=[]
 class MainTask:
 
     newid = itertools.count()
@@ -11,17 +12,25 @@ class MainTask:
         self.start_date = datetime.datetime.now()
         self.end_date = end_date
         self.worker = worker
-
+        data.append(self)
     def __str__(self):
         return "\nTicket ID: {0}\nStart date is: {1}\nDeadline is: {2}\nEnd Date is: {3}\nWho is doing the ticket: {4}\n".format(self.ticket_id, self.start_date.strftime("%x"), self.deadline, self.end_date, self.worker)
 
     def set_deadline(self, date):
+        "Manually set deadline"
         self.deadline = date
 
     def set_ticketid(self, id):
+        """Manually set ticket_id"""
         self.ticket_id = id
 
+class ProgramTask(MainTask):
+    def __init__(self, deadline, end_date, worker, program):
+        super().__init__(deadline, end_date, worker)
+        self.program=program
+        
+    def __str__(self):
+        st=super().__str__()
+        st += 'Program: ' + str(self.program) + "\n"
+        return st
 
-test = MainTask("Tomorrow", "Yesterday", "nico")
-
-print(test)
